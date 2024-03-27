b195db442a74ea934ead877ab6e7421a59276075
Manuel Westermeier <150041048+ManuelWestermeier@users.noreply.github.com>
2024-03-27 10:06:50 +0100

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,63 @@
+import { randomBytes } from "crypto";
+import { createServer } from "http";
+import serverless from "serverless-http";
+
+const cards = {}
+const keys = []
+
+function createPost(title, creater, content) {
+    const id = randomBytes(10).toString("base64url")
+
+    cards[id] = {
+        title,
+        meta: `${new Date().toDateString()} ${creater}`,
+        content
+    }
+
+    keys.push(id)
+
+    return id
+}
+
+createPost("test 001", "me", "Hello World")
+
+const app = createServer((req, res) => {
+
+    res.setHeader("Access-Control-Allow-Origin", "*")
+
+    var url = new URL("http://localhost" + req.url)
+
+    if (url.pathname == "/random") {
+        const id = keys[Math.floor(Math.random() * keys.length)]
+        return res.end(JSON.stringify(
+            {
+                ...cards[id],
+                id
+            }
+        ))
+    }
+
+    if (url.pathname == "/id") {
+        const id = url.searchParams.get("id")
+        return res.end(JSON.stringify(
+            {
+                ...(cards[id] ?? {}),
+                id
+            }
+        ))
+    }
+
+    res.end(
+        createPost(
+            url.searchParams.get("title"),
+            url.searchParams.get("creater"),
+            url.searchParams.get("content")
+        )
+    )
+
+})
+
+process.on("uncaughtException", err => console.log(err))
+
+
+export var handler = serverless(app);
